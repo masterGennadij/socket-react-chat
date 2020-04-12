87d8bafc1e10af4d99aa8215c049a2527ca6ab62
Alexander Makeev <alex.m@myualab.com>
2020-04-12 17:02:30 +0300

    Removed redirects

--- server/index.js
+++ server/index.js
@@ -39,13 +39,6 @@ io.on("connect", (client) => {
 
 // For production static serving
 if (process.env.NODE_ENV === "production") {
-  app.use((req, res, next) => {
-    if (req.secure) {
-      next();
-    } else {
-      res.redirect("https://" + req.headers.host + req.url);
-    }
-  });
   // Serve any static files
   app.use(express.static(path.join(__dirname, "../client/build")));
 
